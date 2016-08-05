139649484e719dc5ca939214a7e792f47bae3c9c
Alberto Segundo <ansegundo@hotmail.com>
2016-08-05 11:36:42 -0300

    Update Minimax.java

--- Minimax.java
+++ Minimax.java
@@ -14,3 +14,28 @@ public class Minimax {
     
     
 }
+
+
+    private boolean limite(int i, int j){
+    	// Limite de Tamanho
+        if(i >= tam || i < 0){
+            //if(tabu.get(i).get(j) == Dama.DBRANCA|| tabu.get(i).get(j) == Dama.BRANCA)
+               //System.out.println("Essa linha não existe");
+            return false;
+        }
+        // Limite de Tamanho
+        if(j >= tam || j < 0){
+            //if(tabu.get(i).get(j) == Dama.DBRANCA|| tabu.get(i).get(j) == Dama.BRANCA)
+                //System.out.println("Essa coluna não existe");
+            return false;
+        }
+    	return true;
+    }
+    
+    private boolean pos_vazia(ArrayList<ArrayList<Dama>>tabu, int i, int j){
+        if(tabu.get(i).get(j) == Dama.VAZIA){
+            return true;
+        }
+        return false;
+    }
+    
